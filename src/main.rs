9852910618022054fa6503cd96ddcbffb50a2f6e
Martin Penckert <martin.penckert@cevt.se>
2022-12-12 12:18:01 +0100

    Adding basic rendering routines

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,31 @@
 // try to achieve the same result!
 extern crate sdl2;
 
+pub mod render;
+
 fn main() {
     let sdl = sdl2::init().unwrap();
     let video_subsystem = sdl.video().unwrap();
+
+    let gl_attr = video_subsystem.gl_attr();
+    gl_attr.set_context_profile(sdl2::video::GLProfile::Core);
+    gl_attr.set_context_version(4, 6);
+
     let window = video_subsystem
         .window("OpenGL :: Shenanigans", 800, 600)
+        .opengl()
         .resizable()
         .build()
         .unwrap();
+
+    let _gl_context = window.gl_create_context().unwrap();
+    let _gl =
+        gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void);
+
+    unsafe {
+        gl::Viewport(0, 0, 800, 600);
+        gl::ClearColor(0.2, 0.3, 0.3, 1.0);
+    }
 
     let mut event_pump = sdl.event_pump().unwrap();
     'main: loop {
@@ -22,6 +39,10 @@ fn main() {
             }
         }
 
-        // TODO: render window contents
+        unsafe {
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
+
+        window.gl_swap_window();
     }
 }
